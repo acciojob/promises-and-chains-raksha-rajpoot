85248a4921956ec347303b55c9599fb443319c91
raksha-rajpoot <rakraj654@gmail.com>
2025-04-19 00:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+ document.getElementById("userForm").addEventListener("submit", function(event) {
+      event.preventDefault(); 
+
+      const name = document.getElementById("name").value.trim();
+      const age = document.getElementById("age").value.trim();
+
+      
+      if (name === "" || age === "") {
+        alert("Please enter valid details.");
+        return;
+      }
+
+      const ageNumber = parseInt(age);
+
+      const checkEligibility = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (ageNumber > 18) {
+            resolve();
+          } else {
+            reject();
+          }
+        }, 4000);
+      });
+
+      checkEligibility
+        .then(() => {
+          alert(`Welcome, ${name}. You can vote.`);
+        })
+        .catch(() => {
+          alert(`Oh sorry ${name}. You aren't old enough.`);
+        });
+    });
